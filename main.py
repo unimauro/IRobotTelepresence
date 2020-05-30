350e493792334d5be78c4cf4b105e46ae375decd
Carlos Mauro Cárdenas Fernández <unimauro@gmail.com>
2020-05-30 12:03:27 -0500

    Telepresence Irobot
    These are the source Code to made a Telepresence Irobot made by 2008 with python 2.x

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import create
+import time
+import sys
+import subprocess
+
+def synth(txt):
+    subprocess.call(["espeak","-w","test.wav","-p","60","-s","140","-v","es",txt],stdout=subprocess.PIPE)
+    subprocess.call(["aplay","test.wav"],stdout=subprocess.PIPE)
+
+if __name__=="__main__":
+#    r=create.Create('/dev/rfcomm1')
+    synth('Robot Activo')
+#    r.demo(4)
+    synth('Demo de Sonido')
+    time.sleep(10)
+#    r.demo()
+    synth('Cerrando Demo de Sonido')
+#    r.close()
+    synth('Cerrando Conexion al Robot')
+	
